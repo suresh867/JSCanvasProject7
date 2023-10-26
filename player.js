a0e2fc8d9490c79c87fc3e5ba96e79773784eb0b
suresh867 <sureshpathak867@gmail.com>
2023-10-26 12:31:54 +0545

    second commit

---
+++ player.js
@@ -0,0 +1,20 @@
+export default class Player{
+    constructor(gameWidth, gameHeight){
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.states = [];
+        this.currentState = this.states[0];
+        this.image = document.getElementById('dogImage');
+        this.width = 200;
+        this.height = 200;
+        this.x = this.gameWidth/2 - this.width/2;
+        this.y = this.gameHeight - this.height;
+        this.frameX = 0;
+        this.frameY = 0;
+    }
+    draw(context){
+        context.drawImage(this.image, 
+            this.frameX * this.width, this.frameY * this.height, this.width, this.width,
+            this.x, this.y, this.width, this.height);
+    }
+}
